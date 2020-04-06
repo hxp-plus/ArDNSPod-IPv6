7b26d4983ef5a1ca6f8f278c8e720bbc3f879b2f
Anker <anker.wang@slicejobs.com>
2020-04-06 12:37:20 +0800

    支持 ipv6 多个子域名批量更新域名记录

--- README.md
+++ README.md
@@ -1,72 +1,30 @@
-# ArDNSPod
+# ArDNSPod-IPv6
 
 基于DNSPod用户API实现的纯Shell动态域名客户端，适配网卡地址。
+万物互联，更新一个宽带下所有已配置域名的设备ipv6地址。
+> 因为电信运营商只动态ipv6地址的前缀，ipv6的后半部分是设备的唯一地址，永远不会变。
 
 # Usage
 
-[详细介绍使用方法的博文](https://blog.csdn.net/Imkiimki/article/details/83794355)（老鸟请略过）
+## dnspod的配置
+1. 在dnspod上增加ipv6的解析记录，请确保配置完成的ipv6地址测试通过可用
+2. [在dnspod上创建密钥token](https://console.dnspod.cn/account/token)
 
-首先要在dnspod网页上新建解析记录，然后获得api token。  
-复制`dns.conf.example`到同一目录下的`dns.conf`并根据你的配置修。  
-在 `ddnspod.sh` 文件开头修改要解析的ip类型(ipv4/ipv6)    
+## 更改`ddnspod-ipv6.sh`配置
+```bash
+# 填入获得密钥ID和密钥token，用逗号拼接
+# LOGIN_TOKEN="密钥ID,密钥token"
+LOGIN_TOKEN="184236,aa786f388f6cd138d5a53427bbf9d731"
 
-执行时直接运行`ddnspod.sh`，支持cron任务。
+# 需要动态解析的域名
+DOMAIN="github.com"
 
-配置文件格式：
-
+# 需要动态解析的子域名，支持多个子域名，所有子域名需要事先配置好可用的ipv6记录
+# SUB_DOMAIN="子域名1 子域名2 子域名3"
+SUB_DOMAIN="router nas phone"
 ```
-# 安全起见，不推荐使用密码认证
-# arMail="test@gmail.com"
-# arPass="123"
-(现在dnspod已经完全废除密码认证的方式了)
-
-# 推荐使用Token认证
-# 按`TokenID,Token`格式填写
-arToken="12345,7676f344eaeaea9074c123451234512d"
-
-# 每行一个域名
-arDdnsCheck "test.org" "subdomain"
-```
-
-# 最近更新
-
-2015/2/24
-
-- 增加token鉴权方式 (by wbchn)
-
-2015/7/7
-
-- 使用D+服务获取域名解析
-
-2016/2/25
-
-- 增加配置文件，分离脚本与配置，适配内网。
-- 加入Mac支持
-- sed脚本POSIX化，可跨平台
-
-2016/3/23
-
-- 进一步POSIX化，支持Mac和大部分Linux发行版
-- 更改配置文件格式
-
-------forked from origin-----
-
-2018-11-07
-
-- 支持选择IP地址类型，包括外网/内网/IPv6
-
-2019-05-24
-
-- 支持IPV6(如你所料，之前所说的支持是假的)
-- 根据网站的更新，api调用使用TLS v1.2，解决了之前调用失败的问题（感谢@lth410 指出）。
-
-2020-01-03
-- 修复了12月31日由于 dnspod API 改动导致的失效。
-
-2020-01-09
-- API返回格式又变了，更改正则表达匹配。
 
 # Credit
 
-Original: anrip
-Forked form anrip/ArDNSPod
+原始作者: anrip ProfFan
+分支来源 imki911/ArDNSPod
---
+++ ddnspod-ipv6.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+
+#################################################
+# ArDNSPod-IPv6 v0.1.0
+# 使用API动态配置DNSPod的域名IPv6记录
+# Github https://github.com/CongAn/ddnspod-ipv6
+# 作者 anker<cong.an@qq.com>
+# 原始作者 anrip<mail@anrip.com>, http://www.anrip.com/ddnspod ; ProfFan Github https://github.com/imki911/ArDNSPod
+#################################################
+
+# 1. Combine your token ID and token together as follows
+LOGIN_TOKEN="184236,aa786f388f6cd138d5a53427bbf9d731"
+
+# 2. Place each domain you want to check as follows
+# you can have multiple checkDomainIP blocks
+DOMAIN="github.com"
+SUB_DOMAIN="router nas phone"
+
+# Global Variables:
+# 新的域名ip
+newDomainIP=""
+# ipv6的前缀
+ipv6Prefix=""
+
+# OS Detection
+case $(uname) in
+  'Linux')
+    echo "OS: Linux"
+    arIpAddress() {
+
+		  # 因为一般ipv6没有nat ipv6的获得可以本机获得
+		  #ifconfig $(nvram get wan0_ifname_t) | awk '/Global/{print $3}' | awk -F/ '{print $1}'
+      #ip addr show dev eth0 | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d' #如果没有nvram，使用这条，注意将eth0改为本机上的网口设备 （通过 ifconfig 查看网络接口）
+      # 获得公网ipv6
+      ip addr show | grep inet6 | grep 'scope global' | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d'
+    }
+    ;;
+  'FreeBSD')
+    echo 'OS: FreeBSD'
+    exit 100
+    ;;
+  'WindowsNT')
+    echo "OS: Windows"
+    exit 100
+    ;;
+  'Darwin')
+    echo "OS: Mac"
+    arIpAddress() {
+      ifconfig -a | grep 'inet6' | grep 'prefixlen 64 autoconf secured' | sed 's/^.*inet6 //' | sed 's/ .*//'
+    }
+    ;;
+  'SunOS')
+    echo 'OS: Solaris'
+    exit 100
+    ;;
+  'AIX')
+    echo 'OS: AIX'
+    exit 100
+    ;;
+  *) ;;
+esac
+
+# Get data
+# arg: type data
+# see Api doc: https://www.dnspod.cn/docs/records.html#
+dnsapi() {
+    #local inter="https://dnsapi.cn/${1:?'Info.Version'}"
+    local inter="https://dnsapi.cn/${1}"
+    local param="login_token=${LOGIN_TOKEN}&format=json&${2}"
+    echo "${inter}?${param}"
+    curl -Ss "${inter}" -d "${param}"
+    # wget --quiet --no-check-certificate --secure-protocol=TLSv1_2 --output-document=- --user-agent=$agent --post-data $param $inter
+}
+
+# Get Domain IP
+# arg: domain
+getDomainIP() {
+    local domainID recordID recordIP
+    local record_type='AAAA'
+    # Get domain ID
+    domainID=$(dnsapi "Domain.Info" "domain=${1}")
+
+    domainID=$(echo $domainID | sed 's/.*{"id":"\([0-9]*\)".*/\1/')
+
+    # Get Record ID
+    recordID=$(dnsapi "Record.List" "domain_id=${domainID}&sub_domain=${2}&record_type=${record_type}")
+
+    recordID=$(echo $recordID | sed 's/.*\[{"id":"\([0-9]*\)".*/\1/')
+
+    # Last IP
+    recordIP=$(dnsapi "Record.Info" "domain_id=${domainID}&record_id=${recordID}&record_type=${record_type}")
+
+    recordIP=$(echo $recordIP | sed 's/.*,"value":"\([0-9a-z\.:]*\)".*/\1/')
+
+    # Output IP
+    case "$recordIP" in
+      [1-9a-z]*)
+        echo $recordIP
+        return 0
+        ;;
+      *)
+        echo "Get Record Info Failed!"
+        return 1
+        ;;
+    esac
+}
+
+# Update domain IP
+# arg: main domain  sub domain
+modifyDomainIP() {
+    local domainID recordID recordRS recordCD recordIP
+
+    # Get domain ID
+    domainID=$(dnsapi "Domain.Info" "domain=${1}")
+    domainID=$(echo $domainID | sed 's/.*{"id":"\([0-9]*\)".*/\1/')
+    #echo $domainID
+    # Get Record ID
+    recordID=$(dnsapi "Record.List" "domain_id=${domainID}&record_type=${record_type}&sub_domain=${2}")
+    recordID=$(echo $recordID | sed 's/.*\[{"id":"\([0-9]*\)".*/\1/')
+    #echo $recordID
+    # Update IP
+    recordRS=$(dnsapi "Record.Modify" "domain_id=${domainID}&sub_domain=${2}&record_type=${record_type}&record_id=${recordID}&record_line=默认&value=${newDomainIP}")
+    recordCD=$(echo $recordRS | sed 's/.*{"code":"\([0-9]*\)".*/\1/')
+    recordIP=$(echo $recordRS | sed 's/.*,"value":"\([0-9a-z\.:]*\)".*/\1/')
+
+    # Output IP
+    if [ "$recordIP" = "$newDomainIP" ]; then
+        if [ "$recordCD" = "1" ]; then
+            echo $recordIP
+            return 0
+        fi
+        # Echo error message
+        echo $recordRS | sed 's/.*,"message":"\([^"]*\)".*/\1/'
+        return 1
+    else
+        echo $recordIP #"Update Failed! Please check your network."
+        return 1
+    fi
+}
+
+# DDNS Check
+# Arg: Main Sub
+checkDomainIP() {
+    local postRS
+    local domainIP=$(getDomainIP $1 $2)
+    newDomainIP="${ipv6Prefix}:"$(echo $domainIP | sed 's/^\([a-f0-9A-F]*:\)\{4\}//')
+
+    if [ $? -eq 0 ]; then
+        echo "domainIP: ${domainIP}"
+        if [ "$domainIP" != "$newDomainIP" ]; then
+            postRS=$(modifyDomainIP $1 $2)
+
+            if [ $? -eq 0 ]; then
+                echo "update to ${postRS} successed."
+                return 0
+            else
+                echo ${postRS}
+                return 1
+            fi
+        fi
+        echo "Last IP is the same as current, no action."
+        return 1
+    fi
+
+    echo ${domainIP}
+}
+
+# ipv6批量动态域名解析
+# Arg: Main Sub Sub Sub ……
+ ddnspod() {
+    local localIP=$(arIpAddress)
+    # 获得ipv6的前缀
+    ipv6Prefix=$(echo $localIP | sed 's/\(:[a-f0-9A-F]*\)\{4\}$//')
+    echo "localIP: ${localIP}"
+    echo "ipv6Prefix: ${ipv6Prefix}"
+
+    # 支持多个子域名
+    if [ -n "$2" ];then
+      for (( i = 2; i <= $#; i++ )); do
+          #statements
+          echo ''
+          echo "Updating Domain: ${!i}.${1}"
+          checkDomainIP $1 ${!i}
+      done
+    fi
+    return 1
+}
+
+ddnspod $DOMAIN $SUB_DOMAIN
